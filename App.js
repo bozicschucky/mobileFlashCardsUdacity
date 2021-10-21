043a11424c4b582dbc6f3c5a97b39a6f59a3cea7
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-10-21 06:34:38 +0100

    set up the app files

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <Text>App running</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
