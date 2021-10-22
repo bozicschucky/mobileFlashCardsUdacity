03e4e966f1d71fe629e6a7b9b648374789d8f583
Ssekitto charlse <bozicslxye1@gmail.com>
2021-10-22 18:44:47 +0300

    Ft add app styles (#4)
    * add all the screens to the main component
    
    * add styles to the app
    
    Co-authored-by: chucky

--- App.js
+++ App.js
@@ -7,67 +7,27 @@
  */
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {Provider} from 'react-redux';
 import {store} from './src/store';
-import HomeScreen from './src/screens/HomeScreen';
-import DeckDetails from './src/screens/DeckDetails';
-import NewCard from './src/screens/NewCard';
-import NewDeck from './src/screens/NewDeck';
-import Quiz from './src/screens/Quiz';
+import MainComponent from './src/screens/Index';
 
-const Stack = createNativeStackNavigator();
-
-const ScreenHeaderStyles = {
-  headerStyle: {
-    backgroundColor: '#ed2c2c',
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    fontWeight: 'bold',
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
   },
 };
-
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Decks"
-          screenOptions={ScreenHeaderStyles}>
-          <Stack.Screen
-            name="Decks"
-            component={HomeScreen}
-            options={{title: 'Decks'}}
-          />
-          <Stack.Screen
-            name="DeckDetails"
-            component={DeckDetails}
-            options={{title: 'Deck Details'}}
-          />
-          <Stack.Screen
-            name="NewCard"
-            component={NewCard}
-            options={{title: 'Add Card'}}
-          />
-          <Stack.Screen
-            name="NewDeck"
-            component={NewDeck}
-            options={{title: 'Add New Deck'}}
-          />
-          <Stack.Screen
-            name="StartQuiz"
-            component={Quiz}
-            options={{title: 'Quiz'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={theme}>
+        <MainComponent />
+      </PaperProvider>
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
