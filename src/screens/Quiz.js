179979f0b884e8557134623600afa375bb797759
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-10-22 11:32:02 +0100

    add logic to handle the quiz component

--- src/screens/Quiz.js
+++ src/screens/Quiz.js
@@ -1,11 +1,93 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import Header from '../components/StatusBar';
-export default function NewDeck() {
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {computePercentage} from '../utils/helpers';
+
+export default function Quiz(props) {
+  const title = props.route.params.title;
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [showQuestion, setShowQuestion] = useState(false);
+  const [showFinalMessage, setShowFinalMessage] = useState(false);
+  const questions = useSelector(state => state.decks[title].questions);
+  const totalQuestions = questions.length;
+  const [Answer, setAnswer] = useState(questions[questionIndex]['question']);
+  const handleShowAnswer = answer => {
+    if (answer === 'answer') {
+      setAnswer(questions[questionIndex]['answer']);
+      setShowQuestion(true);
+    } else {
+      setAnswer(questions[questionIndex]['question']);
+      setShowQuestion(false);
+    }
+  };
+
+  const handleCorrectClick = correctness => {
+    let qtnIndex = questionIndex;
+    qtnIndex++;
+    if (qtnIndex <= totalQuestions - 1) {
+      if (correctness === 'correct') {
+        setCorrectAnswers(correctAnswers + 1);
+        setQuestionIndex(qtnIndex);
+      } else {
+        setQuestionIndex(qtnIndex);
+      }
+    } else {
+      setShowFinalMessage(true);
+    }
+  };
   return (
-    <View>
-      <Header />
-      <Text>Welcome to the display quiz screen</Text>
+    <View style={gameStyles.container}>
+      <View>
+        <Text>
+          {questionIndex + 1}/{totalQuestions}{' '}
+        </Text>
+        <Text>{correctAnswers}</Text>
+        <Text>{Answer}</Text>
+        {showFinalMessage ? (
+          <View>
+            <Text>QUIZ COMPLETED !! </Text>
+            <Text>
+              correctPercentage{' '}
+              {computePercentage(correctAnswers, totalQuestions)}%
+            </Text>
+          </View>
+        ) : (
+          <Text></Text>
+        )}
+
+        {/* <View style={gameStyles.container}> */}
+        {showQuestion ? (
+          <Button
+            title="Question"
+            onPress={() => handleShowAnswer('question')}
+          />
+        ) : (
+          <Button title="Answer" onPress={() => handleShowAnswer('answer')} />
+        )}
+        {/* </View> */}
+      </View>
+      <View>
+        <Button
+          title="Correct"
+          onPress={() => {
+            handleCorrectClick('correct');
+          }}
+        />
+        <Button
+          title="Incorrect"
+          onPress={() => {
+            handleCorrectClick('Incorrect');
+          }}
+        />
+      </View>
     </View>
   );
 }
+
+const gameStyles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
