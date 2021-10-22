acd14fc6b8d714bc7d0182f0dcc2244cef10b55d
chucky <charles.ssekitto@cobblestoneenergy.com>
2021-10-22 12:34:58 +0100

    round off floats

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,5 @@ export const computePercentage = (value, total) => {
   if (value === 0 || total === 0) {
     return 0;
   }
-  console.log('value , total', value, total);
-  return (value / total) * 100;
+  return Math.round((value / total) * 100);
 };
